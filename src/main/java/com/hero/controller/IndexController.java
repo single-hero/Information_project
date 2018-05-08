406592cb431db9d4997fd1f211dbae0ae4446aa2
Single-hero <13411559053@163.com>
2018-05-08 17:39:30 +0800

    项目框架1.1.0 添加了EasyUI,添加Index打字动画

---
+++ src/main/java/com/hero/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.hero.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 接口层-首页跳转
+ * @author hero
+ * @time 2018.05.08
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public void toIndex(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/hero/html/index.html");
+    }
+}
---
+++ src/main/resources/public/hero/js/index.js
@@ -0,0 +1,44 @@
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1200);
+}
+//打字速度
+$(document).ready(function(){
+  autoType(".type-js",100);
+});
+
+
+
+var time = 5; //时间,秒
+var timelong = 0;
+function diplaytime(){ //时间递减
+    document.all.his.innerHTML = time -timelong ;
+    timelong ++;
+}
+function redirect(){ //跳转页
+    window.location.href="Login.html";//指定要跳转到的目标页面
+}
+timer=setInterval('diplaytime()', 1300);//显示时间
+timer=setTimeout('redirect()',time * 1300); //跳转
