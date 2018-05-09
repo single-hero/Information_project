66f9cc74c1b9fd632b5ef7d746328af48533abbe
Single-hero <13411559053@163.com>
2018-05-09 14:54:54 +0800

    项目框架1.1.2 前端登陆模块完成

---
+++ src/main/resources/public/hero/js/login.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    //登陆
+    $('#submit').click(function () {
+        var account=$('#account').val();
+        var password=$('#password').val();
+
+        alert("账号:"+account);
+        alert("密码:"+password);
+    })
+
+})
