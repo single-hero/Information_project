10aed1b7994cc7fd486faa70f9916d72823f9110
Single-hero <13411559053@163.com>
2018-11-01 18:31:53 +0800

    Information 1.5.2 新增极光推送util

--- src/main/java/com/hero/DemoApplication.java
+++ src/main/java/com/hero/DemoApplication.java
@@ -2,7 +2,10 @@ package com.hero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.io.IOException;
 
 /**
  * 启动初始化
@@ -15,13 +18,22 @@ import org.springframework.context.annotation.ComponentScan;
 //@Configuration
 @EnableAutoConfiguration
 public class DemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public static void main(String[] args) throws IOException {
+//		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext ctx=SpringApplication.run(DemoApplication.class, args);
+		System.out.println("\n");
+		//获取字符串
+		System.out.println(ctx.getEnvironment().getProperty("system.name"));
+		//获取整数
+		System.out.println(ctx.getEnvironment().getProperty("system.version",Integer.class));
+		System.out.println("====================================================");
 
-		//测试
-		long maxMemory = Runtime.getRuntime().maxMemory();//返回Java虚拟机试图使用的最大内存量。
-		Long totalMemory = Runtime. getRuntime().totalMemory();//返回Java虚拟机中的内存总量。
-		System.out.println("Java虚拟机试图使用的最大内存量->>>MAX_MEMORY ="+maxMemory +"(字节)、"+(maxMemory/(double)1024/1024) + "MB");
-		System.out.println("Java虚拟机中的内存总量->>>TOTAL_ MEMORY = "+totalMemory +"(字节)"+(totalMemory/(double)1024/1024) + "MB");
+
+
+//		//测试
+//		long maxMemory = Runtime.getRuntime().maxMemory();//返回Java虚拟机试图使用的最大内存量。
+//		Long totalMemory = Runtime. getRuntime().totalMemory();//返回Java虚拟机中的内存总量。
+//		System.out.println("Java虚拟机试图使用的最大内存量->>>MAX_MEMORY ="+maxMemory +"(字节)、"+(maxMemory/(double)1024/1024) + "MB");
+//		System.out.println("Java虚拟机中的内存总量->>>TOTAL_ MEMORY = "+totalMemory +"(字节)"+(totalMemory/(double)1024/1024) + "MB");
 	}
 }
