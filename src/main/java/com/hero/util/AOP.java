86eca6932cf44f2aad342e9edb5209067299eddd
Single-hero <13411559053@163.com>
2018-11-02 20:20:14 +0800

    Information 1.5.3 新增获取客户端信息工具类

--- src/main/java/com/hero/util/AOP.java
+++ src/main/java/com/hero/util/AOP.java
@@ -88,6 +88,7 @@ public class AOP extends BaseController{
         logger.info("调用方法 : " + point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName());//CLASS_METHOD
         logger.info("请求参数 : " + data.toString());
         logger.info("ARGS : " + point.getArgs());
+        logger.info("客户端信息:" +UserClient.UserAgent(request));
 
         //实例化中文转换工具
         ChineseUtill chineseUtill=new ChineseUtill();
