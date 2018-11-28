8810c4780a26c9a09da582a83a06842cd949e59b
Single-hero <13411559053@163.com>
2018-11-28 10:51:15 +0800

    Information 1.6.1 新增swagger2构建restful api生成文档

--- src/main/java/com/hero/controller/LoginController.java
+++ src/main/java/com/hero/controller/LoginController.java
@@ -1,19 +1,29 @@
 package com.hero.controller;
 
 import com.hero.systemBase.PageData;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Api(description = "用户登陆")
 @Controller
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/login",method = RequestMethod.GET)
 public class LoginController extends BaseController{
 
+    @ApiOperation(value = "登陆测试",notes = "只是一个测试")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query",name = "Username",value = "用户名称",required = true,dataType = "String"),
+            @ApiImplicitParam(paramType = "query",name = "Password",value = "用户密码",required = true,dataType = "String"),
+    })
     @RequestMapping(value = "/test")
     @ResponseBody
     public Object test(){
         PageData pd =this.getPageData();
-
         return pd;
     }
 }
---
+++ src/main/java/com/hero/systemBase/Swagger2Config.java
@@ -0,0 +1,41 @@
+package com.hero.systemBase;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * swagger
+ * @author chenwenwei
+ * @time 2018.11.28
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi(){
+        return  new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hero"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("spring Boot 中使用swagger2构建RESTful Apis")
+                .description("后台登陆http://localhost/")
+                .termsOfServiceUrl("服务条款")
+                .contact("chenwenwei")
+                .build();
+    }
+}
